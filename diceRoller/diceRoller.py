1f666298209ace1e59d8403fd7c83c2ce095de77
Bobby Richardson <117216638+tietoa-Bobby@users.noreply.github.com>
2022-11-16 23:03:13 +0000

    Add files via upload

---
+++ diceRoller/diceRoller.py
@@ -0,0 +1,51 @@
+# ● ┌ ─ ┐ │ └ ┘
+
+import random
+
+diceArt = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+numOfDice = int(input("how many dice?"))
+
+for die in range(numOfDice):
+    dice.append(random.randint(1,6))
+
+for die in range(numOfDice):
+    for line in diceArt.get(dice[die]):
+        print(line)
+
+for die in dice:
+    total += die
+print(f"total: {total}")
